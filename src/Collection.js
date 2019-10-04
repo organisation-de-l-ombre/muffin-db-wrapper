d4f55b57bff1119f441cbd3536c2db93c54846f6
Cat66 <36672371+Cat66000@users.noreply.github.com>
2019-10-05 01:25:30 +0200

    Collection : new methods get & set + few changes

--- src/Collection.js
+++ src/Collection.js
@@ -15,19 +15,67 @@ class Collection {
         if (this.client.closed === true) throw new Err("the database has been closed", "MuffinClosedError");
     }
 
-    get(key, path) {
-        [_readyCheck]();
+    async set(key, val, path = null, cache = true) {
+        try {
+            this[_readyCheck]();
 
-        if (_.isNil(key)) return null;
+            if (_.isNil(key)) throw new Err("key is null or undefined");
 
-        if (!_.isNil(path)) {
-            const cachedData = this.cache.get(key);
+            key = key.toString();
 
-            if (_.has(cachedData, path)) {
-                return _.get(cachedData, path);
-            } else {
-                this.base.find()
+            if (path != null) {
+                const result = await this.base.findOne({ _id: key });
+                val = _.set(result.value || {}, path, val);
             }
+
+            if (cache && val !== this.cache.get(key)) {
+                await this.cache.set(key);
+            }
+
+            await this.base.updateOne({ _id: key }, { $set: { _id: key, value: val } }, { upsert: true });
+
+            return this;
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    async get(key, path = null, cache = true) {
+        try {
+            this[_readyCheck]();
+
+            if (_.isNil(key)) return null;
+
+            key = key.toString();
+
+            const result = await this.base.findOne({ _id: key });
+            const data = this.cache.get(key) || result.value;
+
+            if (_.isNil(data)) return null;
+
+            if (cache && data !== this.cache.get(key)) {
+                this.cache.set(key);
+            }
+
+            if (path == null) {
+                return data;
+            } else {
+                return _.get(data, path);
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    has(key, path = null) {
+
+    }
+
+    async ensure(key, path = null, cache = true) {
+        try {
+
+        } catch (e) {
+            console.error(e);
         }
     }
 
