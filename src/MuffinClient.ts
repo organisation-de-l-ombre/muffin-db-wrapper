124e879dcb2e5a99fa00b15cbdac5018d7c76042
Cat66000 <none>
2020-07-06 13:11:41 +0200

    More methods

--- src/MuffinClient.ts
+++ src/MuffinClient.ts
@@ -3,29 +3,50 @@ import MuffinError from "./MuffinError";
 export interface BaseProvider {
 	defer: Promise<void>;
 	isReady: boolean;
-	connect: () => Promise<this>;
+	connect: () => Promise<void>;
 }
 
 export interface ClientOptions {
 	provider: BaseProvider;
+	useCache: boolean;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default class MuffinClient<TKey = any, TValue = any> {
+export class MuffinClient<TKey = any, TValue = any> {
+	provider: BaseProvider;
 	defer: BaseProvider["defer"];
 	cache: Map<TKey, TValue>;
-	provider: BaseProvider;
 
-	constructor({ provider }: ClientOptions) {
+	useCache: boolean;
+
+	constructor({ provider, useCache }: ClientOptions) {
 		if (!provider) {
-			throw new MuffinError("Can not initialize a new MuffinClient without a provider");
+			throw new MuffinError("Can not invoke a new MuffinClient without a provider");
 		}
 
 		this.provider = provider;
 		this.defer = provider.defer;
+
+		this.useCache = useCache || false;
+
+		if (this.useCache) {
+			this.cache = new Map();
+		}
+	}
+
+	private readyCheck() {
+		if (!this.isReady) {
+			throw new MuffinError("The database is not ready");
+		}
 	}
 
 	get isReady(): boolean {
 		return this.provider.isReady;
+	}
+
+	async connect(): Promise<this> {
+		await this.provider.connect();
+
+		return this;
 	}
 }
