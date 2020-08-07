e91e2ae92a07775a306fb1b26bb9ca372ca1eff7
Cat66000 <none>
2020-08-07 20:16:42 +0200

    Add eslint-disable-next-line in index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
 	Client: require("./lib/MuffinClient.js"),
 	Piece: require("./lib/Piece.js"),
+	// eslint-disable-next-line @typescript-eslint/no-var-requires
 	version: require("./package.json").version,
 };
