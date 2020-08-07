39c61ee5ae246c1e93da04ca2fa0c91859eca883
Cat66000 <none>
2020-08-07 05:46:14 +0200

    Almost finished MongoProvider

--- providers/mongodb/src/MongoProvider.ts
+++ providers/mongodb/src/MongoProvider.ts
@@ -1,1 +1,64 @@
-export default class MongoProvider {}
+import { MongoClient, Db, Collection } from "mongodb";
+
+interface ProviderOptions {
+	url?: string;
+	dbName: string;
+	collectionName: string;
+}
+
+export default class MongoProvider<TKey, TValue> {
+	public databaseClient: MongoClient;
+
+	public isReady = false;
+	public isClosed = false;
+
+	public resolveDefer: () => void;
+	public defer: Promise<void>;
+
+	public db: Db;
+	public coll: Collection<{ _id: TKey; value: TValue }>;
+
+	constructor(public options: ProviderOptions) {
+		const url = options.url || `mongodb+srv://localhost:27017/?retryWrites=true&w=majority`;
+
+		this.databaseClient = new MongoClient(url, { useNewUrlParser: true });
+		this.db = this.databaseClient.db(options.dbName);
+		this.coll = this.db.collection(options.collectionName);
+
+		this.defer = new Promise((res) => {
+			this.resolveDefer = res;
+		});
+	}
+
+	public async connect(): Promise<void> {
+		await this.databaseClient.connect();
+	}
+
+	public async close(): Promise<void> {
+		await this.databaseClient.close();
+	}
+
+	public size(): Promise<number> {
+		return this.coll.countDocuments();
+	}
+
+	public async clear(): Promise<void> {
+		await this.coll.drop();
+	}
+
+	public async delete(key: TKey): Promise<boolean> {
+		return (await this.coll.deleteOne({ _id: key })).deletedCount > 0;
+	}
+
+	public async entryArray(): Promise<[TKey, TValue][]> {
+		return (await this.fetchAll()).map(({ _id, value }) => [_id, value]);
+	}
+
+	public fetch(key: TKey): Promise<TValue> {
+		return this.coll.findOne({ _id: key });
+	}
+
+	private fetchAll(): Promise<{ _id: TKey; value: TValue }[]> {
+		return this.coll.find({}).toArray();
+	}
+}
