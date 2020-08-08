af6f60207c82a8f6d3ab9c7117f31d7a273d03d2
Cat66000 <none>
2020-08-08 22:12:58 +0200

    Fix index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = {
 	Client: require("./lib/MuffinClient.js"),
-	Piece: require("./lib/Piece.js"),
 	// eslint-disable-next-line @typescript-eslint/no-var-requires
 	version: require("./package.json").version,
 };
